6a43bf6eaa2054f386f3dd642792087201af72a0
Fernando Garcia De Castro <garciaf4c@gmail.com>
2017-11-08 12:54:21 +0100

    commit 4

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -1,72 +1,57 @@
 
 public class CalendarioBasico
 {
-
-    private int dia ;
-    private int mes;
-    private int año;
+    //declarando variables dia mes y año
+    private DisplayDosCaracteres dia;
+    private DisplayDosCaracteres mes;
+    private DisplayDosCaracteres ano;
 
     /**
      * Constructor de la  class CalendarioBasico
      */
     public CalendarioBasico()
     {
-        dia = 1;
-        mes = 1;
-        año = 1;
+        //se pone 31 por que solo llega hasta 30 y no lo usa el ultimo numer que se ponga
+        dia = new DisplayDosCaracteres (31);
+        mes = new DisplayDosCaracteres (13);
+        ano = new DisplayDosCaracteres (100);
     }
 
     /**
      * Fijar fecha
      */
-    public void fijarFecha(int nuevoDia,int nuevoMes, int nuevoAño)
+    public void fijarFecha(int nuevoDia,int nuevoMes, int nuevoAno)
     {
-        dia = nuevoDia;
-        mes = nuevoMes;
-        año = nuevoAño;
+        // fijar la fecha teniendo encuena la clase nueva creada segun el metodo en la clase DisplayDosCaracteres
+        dia.setValorAlmacenado(nuevoDia);
+        mes.setValorAlmacenado(nuevoMes);
+        ano.setValorAlmacenado(nuevoAno);
     }
 
     /**
      * metodo para obtener la fecha
      */
     public String obtenerFecha()
-    {
-        String textoADevolver="";
-        //variables locales por cada parte de textoadevolver
-        String parte1 = dia + "";
-        String parte2 = mes + "";
-        String parte3 = año + "";
-        //cuanto mide de largo la parte 1 y si no mide 2 le añade un cero delante
-        if (parte1.length()<2){
-            parte1 = "0" + parte1;   
-        }
-        //hasta septiembre por que es 09 y luego 10
-        if (mes < 10){
-            parte2= "0" + parte2;  
-        }
-        // hasta el año 09 por que no tiene 2 digitos
-        if (año < 10 ){
-            parte3 = "0" + parte3;  
-        }
-        textoADevolver = parte1 + "-" + parte2 + "-" + parte3 ;
-        return textoADevolver;
+    { 
+        return dia.getTextoDelDisplay() + "-" + mes.getTextoDelDisplay() + "-" + ano.getTextoDelDisplay();
     }
 
     /**
-     * Añadir un dia mas, avanzar un dia
+     * Añadir un dia mas, avanzar un dia.
+     * 
      */
     public void avanzarFecha()
     {
-        dia = dia +1;
-        if (dia == 31){
-
-            dia = 1;
-            mes = mes +1;
         
-            if (mes == 13){
-                mes = 1;
-                año = año +1;
+        if (dia.getValorAlmacenado () == 30)
+        {
+            mes.incrementaValorAlmacenado();  
+            if(mes.getValorAlmacenado() == 1)
+            {
+                ano.incrementaValorAlmacenado();
+
             }
         }
+        dia.incrementaValorAlmacenado();
     }
 }
